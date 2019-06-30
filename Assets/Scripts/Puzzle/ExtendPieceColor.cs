36a475684e24cd5c55c5f1c84e6a0dd74e051f6d
tokkq <tokkocode@gmai.com>
2019-06-30 16:56:24 +0900

    MonsterにColor追加。ExtendPieceColor追加。

---
+++ Assets/Scripts/Puzzle/ExtendPieceColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Puzzle
+{
+    public static class ExtendPieceColor
+    {
+        public static Color ToColor(this PieceColor pieceColor)
+        {
+            switch (pieceColor)
+            {
+                case PieceColor.Red:
+                    return Color.red;
+                case PieceColor.Green:
+                    return Color.green;
+                case PieceColor.Blue:
+                    return Color.blue;
+                case PieceColor.Yellow:
+                    return Color.yellow;
+                default:
+                    throw new ArgumentOutOfRangeException($"{pieceColor} dosent set color.");
+            }
+        }
+    }
+}
--- Assets/Scripts/Tower/Monster/MonsterViewFactory.cs
+++ Assets/Scripts/Tower/Monster/MonsterViewFactory.cs
@@ -1,4 +1,5 @@
-﻿using Tower.Lane;
+﻿using Puzzle;
+using Tower.Lane;
 using UnityEngine;
 
 namespace Tower.Monster
@@ -20,6 +21,8 @@ namespace Tower.Monster
             var spawnPosition = _laneViewContainer.GetLaneViewFromLaneNumber(lane).MonsterSpawnPosition;
             var createdView = Object.Instantiate(monsterView, spawnPosition, Quaternion.AngleAxis(180, Vector3.up));
             createdView.gameObject.SetActive(true);
+            var randomPieceColor = EnumCommon.Random<PieceColor>();
+            createdView.SetColor(randomPieceColor);
             return createdView;
         }
     }
