59b7f744f8b467be8ef7623cbbc10360666e6d51
wkno115 <dendennaoto@gmail.com>
2019-07-03 20:04:42 +0900

    全体設計の指針

---
+++ Assets/Scripts/Subscene/SubsceneDirector.cs
@@ -0,0 +1,26 @@
+﻿using Play;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Subscene
+{
+    public class SubsceneDirector : MonoBehaviour
+    {
+        [SerializeField]
+        PlaySubsceneDirector _playSubsceneDirector;
+        
+
+        // Start is called before the first frame update
+        IEnumerator Start()
+        {
+            yield return null;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
